701df7ae22a942c8cde341125419491cb49e183e
Andy Peters <devel@latke.net>
2025-03-30 23:47:34 -0700

    create top-level file.

---
+++ src/ltc_mtc.vhdl
@@ -0,0 +1,25 @@
+-------------------------------------------------------------------------------
+-- Title      : Linear Time Code / MIDI Time Code conversion
+-- Project    : 
+-------------------------------------------------------------------------------
+-- File       : ltc_mtc.vhdl
+-- Author     : Andy Peters  <devel@latke.net>
+-- Company    : ASP Digital
+-- Created    : 2025-03-30
+-- Last update: 2025-03-30
+-- Platform   : 
+-- Standard   : VHDL'08, Math Packages
+-------------------------------------------------------------------------------
+-- Description: Convert between Linear Time Code and MIDI Time Codexs
+-------------------------------------------------------------------------------
+-- Copyright (c) 2025 ASP Digital
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-- 2025-03-30  -        andy	Created
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+
